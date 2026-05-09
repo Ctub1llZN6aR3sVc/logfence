d23a359118b3f9bea4ed38bcca5682d94162d178
Carter Billings <Ctub1llZN6aR3sVc@users.noreply.github.com>
2026-05-10 03:40:44 +0800

    test: expose Len and Head helpers in export_test.go for white-box testing

--- internal/buffer/export_test.go
+++ internal/buffer/export_test.go
@@ -4,3 +4,13 @@ package buffer
 func (r *Ring) Cap() int {
 	return r.cap
 }
+
+// Len exposes the current number of entries stored for white-box tests.
+func (r *Ring) Len() int {
+	return r.len
+}
+
+// Head exposes the current head index for white-box tests.
+func (r *Ring) Head() int {
+	return r.head
+}
